4c80da0edce45ebca5388b6433785d9b0e877458
gluver <credog@foxmail.com>
2021-06-20 22:03:46 +0300

    changed defult video path for extracting features

---
+++ mynote.md
@@ -0,0 +1,10 @@
+# Todo list
+- 弄清模型工作机制
+    - 描述生成教程github仓库[ImageCaptioning.pytorch](https://github.com/ruotianluo/ImageCaptioning.pytorch)
+    - [视频描述生成(Video Captioning)简介](https://zhuanlan.zhihu.com/p/118166500)
+- 厘清项目依赖（coco-caption）
+- 仓库中代码如何训练模型
+- 导入训练数据，运行训练脚本
+- 如何生成模型评估指标
+- cocoscorer 如何使用
+- prepro_vocab,
--- prepro_feats.py
+++ prepro_feats.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
                         help='how many frames to sampler per video')
 
     parser.add_argument("--video_path", dest='video_path', type=str,
-                        default='data/train-video', help='path to video dataset')
+                        default='../input/vcap-data-cctv-news/cpation_dataset/Train/G_15000-G_17249/video', help='path to video dataset')
     parser.add_argument("--model", dest="model", type=str, default='resnet152',
                         help='the CNN model you want to use to extract_feats')
     
